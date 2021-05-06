9e3ae68a5528b6616d275a327819e44aac3c7906
mehdaoui-fatima <mehdaoui.fa@gmail.com>
2021-05-06 15:20:28 +0000

    mysql and wordpress telegraf added

---
+++ srcs/mysql/srcs/services_status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+openrc default
+rc-service mariadb start
+rc-service telegraf start
+while sleep 60; do
+pgrep nginx > /dev/null
+status1=$?
+pgrep telegraf >> /dev/null
+status2=$?
+if [ $status1 !=  0 ]
+then
+   rc-service mariadb start
+fi
+if [ $status2 != 0 ]; then
+   rc-service telegraf restart
+fi
+done
+ 
---
+++ srcs/wordpress/srcs/services_status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+openrc default
+rc-service nginx start 
+rc-service php-fpm7 start
+rc-service telegraf start
+while sleep 60; do
+pgrep nginx >> /dev/null
+status1=$?
+pgrep php-fpm7 >> /dev/null
+status2=$?
+pgrep telegraf >> /dev/null
+status3=$?
+if [ $status1 != 0 ]
+then
+    rc-service nginx restart 
+fi
+if [ $status2 != 0 ]
+then
+    rc-service php-fpm7 restart
+fi
+if [ $status3 != 0 ]; then
+    rc-service telegraf restart
+fi
+done
+ 
